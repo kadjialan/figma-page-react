2f4226abf03f8fdc33af066bb3a73275a31fb7d2
kadjialan <alankadji7@gmail.com>
2023-01-10 20:45:16 +0100

    basic react layout

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="leftpart" >
+        <span><i class="fa-sharp fa-solid fa-phone"></i>+250 784 688 641</span>
+        <span><i class="fa-solid fa-envelope"></i>bbonteemma@gmail.com</span>
+      </div>
+    </div>
+  )
+}
+
