cc35b37f0ce576e087b485af6f198afde7b15971
kadjialan <alankadji7@gmail.com>
2023-01-14 02:12:24 +0100

    final home design

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../../components/Navbar/Navbar';
 import Searchnav from '../../components/Searchnav/Searchnav';
 import Cards from './Cards/Cards';
 import Destinations from './Destinations/Destinations';
+import Footer from './Footer/Footer';
 import Hero from './Hero/Hero';
 import Subsearch from './Subsearch/Subsearch';
 import Testimonials from './Testimonials/Testimonials';
@@ -17,6 +18,7 @@ export default function Home() {
       <Cards />
       <Destinations />
       <Testimonials />
+      <Footer />
     </div>
   );
 }
