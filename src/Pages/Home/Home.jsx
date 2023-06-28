9371c9bd7ccd9ebd0a2571f721742f19cc434d79
kadjialan <alankadji7@gmail.com>
2023-06-28 22:46:08 +0100

    deploying

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import Searchnav from '../../components/Searchnav/Searchnav';
-import Bg from './Bg';
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Searchnav />
-      <Bg />
     </div>
   );
 }
